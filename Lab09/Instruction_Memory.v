12edc991393a70800828c7214e9a16c88a071272
shakaibsaleem <ms01036@st.habib.edu.pk>
2018-11-07 13:53:50 +0500

    Update Lab09/Instruction_Memory.v

--- Lab09/Instruction_Memory.v
+++ Lab09/Instruction_Memory.v
@@ -8,22 +8,22 @@ module Instruction_Memory
 	
 	initial
 	begin
-		Memory[0] =8'h10;
-		Memory[1] =8'h32;
-		Memory[2] =8'h54;
-		Memory[3] =8'h76;
-		Memory[4] =8'h98;
-		Memory[5] =8'hBA;
-		Memory[6] =8'hDC;
-		Memory[7] =8'hFE;
-		Memory[8] =8'h10;
-		Memory[9] =8'h32;
-		Memory[10]=8'h54;
-		Memory[11]=8'h76;
-		Memory[12]=8'h98;
-		Memory[13]=8'hBA;
-		Memory[14]=8'hDC;
-		Memory[15]=8'hFE;
+		Memory[0] =8'b10000011;
+		Memory[1] =8'b00110100;
+		Memory[2] =8'b10000101;
+		Memory[3] =8'b00000010;
+		Memory[4] =8'b10110011;
+		Memory[5] =8'b10000100;
+		Memory[6] =8'b10011010;
+		Memory[7] =8'b00000000;
+		Memory[8] =8'b10010011;
+		Memory[9] =8'b10000100;
+		Memory[10]=8'b00010100;
+		Memory[11]=8'b00000000;
+		Memory[12]=8'b00100011;
+		Memory[13]=8'b00110100;
+		Memory[14]=8'b10010101;
+		Memory[15]=8'b00000010;
 	end
 	
 	always@ *
